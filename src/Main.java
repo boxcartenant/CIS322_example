82fc72cc61995e9525e0afe3f9cfe35e4c17d1e4
boxcartenant <zactheslade@gmail.com>
2025-02-26 08:23:03 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,15 @@ import ExplorationGame.ExplorationGame;
 
 public class Main {
     public static void main(String[] args) {
-        //create the JFrame to hold all our graphical objects
+        //We're using swing for our graphics
+        //invokelater puts our explorationgame in another thread
         SwingUtilities.invokeLater(() -> {
+            //this jframe is the window that our game will be in.
             JFrame frame = new JFrame("Exploration Game");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             //ExplorationGame extends JPanel, so we can add it to the frame
             frame.add(new ExplorationGame());
+            //pack prepares the frame for rendering
             frame.pack();
             //center it on the display
             frame.setLocationRelativeTo(null);
